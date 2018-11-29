aae6b6432e32b00a80861976921fc9a48ce7c591
checkmarx-sample <checkmarx.sample@gmail.com>
2018-11-28 22:15:38 -0500

    Add existing file

---
+++ tradeportal/WEB-INF/src/java/com/cxsample/tradeportal/CxSampleSanitizer.java
@@ -0,0 +1,14 @@
+package com.cxsample.tradeportal;
+public class CxSampleSanitizer 
+{
+	public static String CxSampleSanitizer(String cleanMe)
+	{
+		String cleansedString ="";
+		
+		cleansedString=cleanMe;
+		
+		return cleansedString;
+	
+	}
+
+}
